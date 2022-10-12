8af96beec57bbbf6067c00d99b3f8bebb5117564
Siddharth Mishra <56888544+geeksid1800@users.noreply.github.com>
2022-10-12 18:53:47 +0530

    0.1
    CNN architecture

---
+++ cnn.py
@@ -0,0 +1,68 @@
+from torch import nn
+from torchsummary import summary
+
+class CNNNetwork(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # 4 conv blocks / flatten / linear / softmax
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.flatten = nn.Flatten()
+        self.linear = nn.Linear(128 * 9 * 33, 10) #As seen from the model summary
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input_data):
+        x = self.conv1(input_data)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.flatten(x)
+        x = self.linear(x) #logits
+        #x = self.softmax(x) #predictions, if using CrossEntropy, softmax not needed
+        return x
+if __name__ == "__main__":
+    cnn = CNNNetwork()
+    #summary(cnn, (1, 128, 500))
